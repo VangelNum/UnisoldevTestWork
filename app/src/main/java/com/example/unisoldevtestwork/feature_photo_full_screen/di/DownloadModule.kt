04595428f23ca629441015b17958eaeda5c682e6
VangelNum <vangelnum@gmail.com>
2023-06-15 01:38:58 +0400

    improved the code for installing wallpapers

---
+++ app/src/main/java/com/example/unisoldevtestwork/feature_photo_full_screen/di/DownloadModule.kt
@@ -0,0 +1,21 @@
+package com.example.unisoldevtestwork.feature_photo_full_screen.di
+
+import android.app.DownloadManager
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DownloadModule {
+
+    @Singleton
+    @Provides
+    fun provideDownloadManager(@ApplicationContext context: Context): DownloadManager {
+        return context.getSystemService(DownloadManager::class.java)
+    }
+}
